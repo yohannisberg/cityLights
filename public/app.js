6a0d144c272df9dd397ab1582b425b65f0f2087d
John D'Agostino <jdagos3@gmail.com>
2017-07-26 17:06:14 -0700

    first commit

---
+++ public/app.js
@@ -0,0 +1,13 @@
+angular.module('cityLights', ["ui.router"])
+    .config(function ($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: './views/home.html',
+                controller: 'homeCtrl'
+            })
+
+    });
---
+++ public/js/controllers/homeCtrl.js
@@ -0,0 +1,12 @@
+angular.module('cityLights').controller('homeCtrl', function ($scope) {
+
+// $scope.getPhotos = () => {
+//   mainService.getPhotos().then(res => {
+//     console.log('yes')
+//     $scope.photo=res.data.data;
+//   })
+// }
+//
+// $scope.getPhotos()
+
+});
